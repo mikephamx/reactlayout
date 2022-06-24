62f35294ec94f7f5adbc4fc75fd6782e719c2850
mikepham <mikepham137@gmail.com>
2022-06-24 23:43:42 +0700

    simple way to render rows with data from an array using Loop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import LayoutExcercise from './component/LayoutExcercise';
+// import LayoutExcercise from './component/LayoutExcercise';
 import RenderWithLoop from './RenderWithLoop/RenderWithLoop';
 
 function App() {
   return (
     <div className="App">
-     <LayoutExcercise/>
+     {/* <LayoutExcercise/> */}
      <RenderWithLoop/>
     </div>
   );
--- src/RenderWithLoop/RenderWithLoop.js
+++ src/RenderWithLoop/RenderWithLoop.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react'
 
 export default class RenderWithLoop extends Component {
+  productList = [
+    { id: 1, name: "black-car", price: 1000, img: './CarBasic/products/black-car.jpg' },
+    { id: 2, name: "red-car", price: 1000, img: './CarBasic/products/red-car.jpg' },
+    { id: 3, name: "silver-car", price: 1000, img: './CarBasic/products/silver-car.jpg' },
+    { id: 4, name: "steel-car", price: 1000, img: './CarBasic/products/steel-car.jpg' },
+  ]
+
+  renderTable = () => {
+    let trComponentArray = [];
+    for (let index = 0; index < this.productList.length; index++) {
+      let product = this.productList[index];
+      //jsx object 
+      let trJSX = (
+        //each row must have a unique key to avoid error
+      <tr key={index}> 
+          <td> {product.id}</td>
+          <td> {product.name}</td>
+          <td> {product.price}</td>
+          <td>
+            <img style={{ width: "100px" }} src={product.img} alt="" />
+          </td>
+          <td></td>
+        </tr>
+      )
+      console.log("trJsx", trJSX);
+      trComponentArray.push(trJSX);
+    }
+    return trComponentArray;
+  }
   render() {
     return (
-      <div>RenderWithLoop</div>
+      <div>
+        {/* <img src={this.productList[0].img} alt=""/> */}
+        <div className='container'>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>name</th>
+                <th>price</th>
+                <th>img</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.renderTable()}
+            </tbody>
+          </table>
+
+        </div>
+      </div>
     )
   }
 }
