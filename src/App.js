15500fb08dbf21220b2d0e65d53e599855d52b1f
Manh Pham <mikepham137@gmail.com>
2022-06-29 16:09:07 +0700

    Create new Project to render films from json file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
+import FilmHeader from './FilmRenderExercise/FilmHeader';
+import FilmRender from './FilmRenderExercise/FilmRender';
 // import LayoutExcercise from './component/LayoutExcercise';
-import RenderWithLoop from './RenderWithLoop/RenderWithLoop';
+// import RenderWithLoop from './RenderWithLoop/RenderWithLoop';
 
 function App() {
   return (
     <div className="App">
      {/* <LayoutExcercise/> */}
-     <RenderWithLoop/>
+     {/* <RenderWithLoop/> */}
+     <FilmHeader/>
+     <FilmRender/>
     </div>
   );
 }
---
+++ src/FilmRenderExercise/FilmRender.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+// import DataFilms from 'reactlayout/src/Data/DataFilms.json'
+
+export default class FilmRender extends Component {
+  render() {
+    return (
+      <div>
+    
+      </div>
+    )
+  }
+}
