c9b9f3575def7c368778d1bab58340f7370a1bee
Manh Pham <mikepham137@gmail.com>
2022-07-04 23:38:30 +0700

    create keyframe for animation

--- src/GlassRender/GlassRender.js
+++ src/GlassRender/GlassRender.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import dataGlasses from '../Data/dataGlasses.json'
+import dataGlasses from '../Data/dataGlasses.json';
+import './GlassRenderCSS.css';
 
 export default class GlassRender extends Component {
     state = {
@@ -14,22 +15,36 @@ export default class GlassRender extends Component {
 
     renderGlassesList = () => {
         return dataGlasses.map((glassItem, index) => {
-            return <img onClick={() => {this.changeGlasses(glassItem)}} className="ml-2 p-2 border border-width-1" src={glassItem.url} key={index} style={{ width: "100px", cursor: 'pointer' }} alt={glassItem.name} />
+            return <img onClick={() => { this.changeGlasses(glassItem) }} className="ml-2 p-2 border border-width-1" src={glassItem.url} key={index} style={{ width: "100px", cursor: 'pointer' }} alt={glassItem.name} />
         })
     }
 
-    changeGlasses = (newGlasses)  => {
+    changeGlasses = (newGlasses) => {
         this.setState({
             glassesCurrent: newGlasses
         })
-     }
+    }
 
     render() {
+        const keyFrame = `
+        @keyframes glassesChangeAnimation${Date.now()} {
+            from{
+                width: 0;
+                transfrom: rotate(45deg);
+            }
+            to {
+                width: 150px;
+                transform: rotate(0deg);
+            }
+        }`;
+
         const styleGlass = {
             width: "150px",
             top: "75px",
             right: "85px",
-            opacity: '0.7'
+            opacity: '0.7',
+            transform: 'rotate(0deg)',
+            animation: `glassesChangeAnimation${Date.now()} 1s`
         }
         const glassInfoStyle = {
             right: '35px',
@@ -43,6 +58,9 @@ export default class GlassRender extends Component {
         }
         return (
             <div style={{ backgroundImage: "url(./glassesImage/background.jpg)", minHeight: '2000px', backgroundSize: '1800px' }}>
+                <style> 
+                    {keyFrame}
+                </style>
                 <div style={{ backgroundColor: 'rgba(0,0,0,0.5', minHeight: '2000px' }}>
                     <h3 className='text-center text-light'>GLASS APP ONLINE</h3>
                     <div className='container'>
@@ -50,7 +68,7 @@ export default class GlassRender extends Component {
                         <div className='row mt-5 justify-content-center'>
                             <div className='col-6 position-relative'>
                                 <img src='./glassesImage/model.jpg' alt="Model" style={{ width: '250px' }} className="position-absolute" />
-                                <img style={styleGlass} src={this.state.glassesCurrent.url} alt="default glasses" className="position-absolute" />
+                                <img style={styleGlass} src={this.state.glassesCurrent.url} alt="default glasses" className="position-absolute glassesStyle" />
                                 <div className='position-absolute' style={glassInfoStyle}>
                                     <span>{this.state.glassesCurrent.name}</span> <br />
                                     <span>{this.state.glassesCurrent.desc}</span>
